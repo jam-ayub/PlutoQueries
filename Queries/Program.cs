c28675e6707d950eea6bcafb1a4ee943143e436f
Jam <jamayub098@gmail.com>
2024-11-16 17:14:06 +0500

    Ordering LINQ Syntax

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -11,19 +11,11 @@ namespace Queries
 
             // LINQ Syntax
             var query = from c in context.Courses 
-                        where c.Name.Contains("c#")
-                        orderby c.Name
+                        where c.Author.Id == 1
+                        orderby c.Level descending, c.Name
                         select c;
 
-            //foreach (var course in query)
-            //    Console.WriteLine(course.Name);
-
-            // Extention Methods
-            var courses = context.Courses
-                .Where(c => c.Name.Contains("c#"))
-                .OrderBy(c => c.Name);
-
-            foreach (var course in courses)
+            foreach (var course in query)
                 Console.WriteLine(course.Name);
 
             Console.ReadLine();
