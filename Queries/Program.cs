a8e5229b60a248855a4bf38d6c919418352fc3a4
Jam <jamayub098@gmail.com>
2024-11-16 18:36:47 +0500

    LINQ Extention Methods | Group Join

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,19 +10,19 @@ namespace Queries
             var context = new PlutoContext();
 
             // Extention Methods Syntax
-            var query = context.Courses.Join(
-                context.Authors, 
-                c => c.AuthorId, 
+            var query = context.Authors.GroupJoin(
+                context.Courses, 
                 a => a.Id, 
-                (course, author) => new 
-                    {
-                        CourseName = course.Name,
-                        AuthorName = author.Name,
-                    });
+                c => c.AuthorId,
+                (author, course) => new
+                {
+                    AuthorName = author.Name,
+                    Courses = course.Count()
+                });
 
             foreach (var group in query)
             {
-                Console.WriteLine("{0} - ({1})", group.AuthorName, group.CourseName);
+                Console.WriteLine("{0} - ({1})", group.AuthorName, group.Courses);
             }
 
             Console.ReadLine();
