93cfa9e45c66b717e076c51c625ca03c31b9a51f
Jam <jamayub098@gmail.com>
2024-11-16 17:00:38 +0500

    LINQ Syntax example

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,10 +1,24 @@
-﻿
+﻿using System;
+using System.Linq;
+
 namespace Queries
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var context = new PlutoContext();
+
+            // LINQ Syntax
+            var query = from c in context.Courses 
+                        where c.Name.Contains("c#")
+                        orderby c.Name
+                        select c;
+
+            foreach (var course in query)
+                Console.WriteLine(course.Name);
+
+            Console.ReadLine();
         }
     }
 }
