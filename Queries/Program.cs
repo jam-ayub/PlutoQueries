9fcc8796bea669d0643035c0a64d36f71f312e33
Jam <jamayub098@gmail.com>
2024-11-16 18:09:28 +0500

    LINQ Extention Methods | Restriction

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -9,14 +9,12 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            // LINQ Syntax
-            var query = from a in context.Authors
-                        from c in context.Courses
-                        select new { AuthorName = a.Name, CourseName = c.Name };
+            // Extention Methods Syntax
+            var query = context.Courses.Where(c => c.Level == 1);
 
             foreach (var group in query)
             {
-                Console.WriteLine("{0} - ({1})", group.AuthorName, group.CourseName);
+                Console.WriteLine("{0} - ({1})", group.Level, group.Name);
             }
 
             Console.ReadLine();
