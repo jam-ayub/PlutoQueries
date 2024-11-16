6fb5eff1e898ba64d08bf68247870426af511b22
Jam <jamayub098@gmail.com>
2024-11-16 18:11:52 +0500

    LINQ Extention Methods | Ordering

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -10,7 +10,10 @@ namespace Queries
             var context = new PlutoContext();
 
             // Extention Methods Syntax
-            var query = context.Courses.Where(c => c.Level == 1);
+            var query = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.Level);
 
             foreach (var group in query)
             {
